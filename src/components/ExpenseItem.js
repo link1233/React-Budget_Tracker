e66e880308c1329cf00c4268f7b6c73467b8026e
johnathanwilson <jjwilsonart@gmail.com>
2023-09-20 17:26:48 +0200

    first components added, basic css styling added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 21st 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$345.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
