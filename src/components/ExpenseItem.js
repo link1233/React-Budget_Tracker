8db38b9b2a48869888e961f26547f070f55821a7
johnathanwilson <jjwilsonart@gmail.com>
2023-09-25 16:04:24 +0200

    added more components and styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
-function ExpenseItem() {
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <div>March 21st 2023</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$345.67</div>
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
